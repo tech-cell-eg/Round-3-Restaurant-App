6d378ff4d606bfe6fc7aa3c92af403242fc38ac7
Belal Eid <91228516+belaleid5@users.noreply.github.com>
2025-04-16 14:59:57 -0700

    update routes

--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -1,9 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:food_app/features/home/presentation/view/home_view.dart';
+
 class Routes {
   static const String main = "main";
-  static const String auth = "auth";
+  static const String login = "auth";
+  static const String home = "auth";
   static const String basket = "basket";
   static const String allDishes = "all-dishes";
   static const String onboarding = "onboarding";
   static const String trackOrder = "track-order";
   static const String confirmOrder = "confirm-order";
+
+
+
+
+   static MaterialPageRoute onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case login:
+      return MaterialPageRoute(builder: (_) => LoginScreen());
+
+    case home:
+      return MaterialPageRoute(builder: (_) => HomeView());
+
+    default:
+      return MaterialPageRoute(builder: (_) => Scaffold());
+  }
 }
+}
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_app/core/constants/routes.dart';
 
-import 'features/auth/cubit/auth/auth_cubit.dart';
-import 'features/auth/cubit/repo/repo.dart';
-import 'features/auth/presentation/screens/forget_password.dart';
-import 'features/auth/presentation/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,23 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: 'Food Delivery',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
       ),
-      routes: {
-        '/':
-            (context) => BlocProvider(
-              create: (context) => AuthCubit(AuthRepository(Dio())),
-              child: const LoginScreen(),
-            ),
-        '/forgot-password':
-            (context) => BlocProvider(
-              create: (context) => AuthCubit(AuthRepository(Dio())),
-              child: const ForgotPasswordScreen(),
-            ),
-      },
+          onGenerateRoute:Routes.onGenerateRoute,
+      
     );
   }
 }
