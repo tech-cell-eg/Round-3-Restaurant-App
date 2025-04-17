644a8cf588bba1fe021252930d2970a9761f2e5a
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-04-17 19:14:44 +0200

    feat: create onboarding screen

---
+++ lib/features/onboarding/model/onboarding_model.dart
@@ -0,0 +1,9 @@
+class OnboardingModel {
+  final String title, description, image;
+
+  OnboardingModel({
+    required this.title,
+    required this.image,
+    required this.description,
+  });
+}
