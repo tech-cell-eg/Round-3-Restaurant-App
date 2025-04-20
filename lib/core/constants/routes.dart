7b214fcdad458507c040ff805ac1218f555d4dc8
Meroothman <147217091+Meroothman@users.noreply.github.com>
2025-04-20 22:01:52 +0200

    update routes

--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -1,6 +1,9 @@
 import 'package:food_app/empty.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/features/auth/presentation/screens/forget_password.dart';
 import 'package:food_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:food_app/features/auth/presentation/screens/sign_up_screen.dart';
+import 'package:food_app/features/auth/presentation/screens/verifiy_screen.dart';
 import 'package:food_app/features/food_burger/presentation/views/food_burger_view.dart';
 import 'package:food_app/features/home/presentation/view/home_view.dart';
 import 'package:food_app/features/search/presentation/view/search_view.dart';
@@ -10,9 +13,13 @@ class Routes {
   static const home = "home";
   static const empty = "empty";
   static const login = "login";
+  static const signup = "signup";
+  static const forgotPassword = "forgot_password";
+  static const verifyEmail = "verify_email";
   static const search = "search";
   static const onboard = "onboard";
   static const foodBurger = "food_burger";
+
   static MaterialPageRoute onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case empty:
@@ -29,6 +36,15 @@ class Routes {
 
       case login:
         return MaterialPageRoute(builder: (_) => LoginScreen());
+
+      case signup:
+        return MaterialPageRoute(builder: (_) => SignUpScreen());
+
+      case forgotPassword:
+        return MaterialPageRoute(builder: (_) => ForgetPassword());
+
+      case verifyEmail: 
+        return MaterialPageRoute(builder: (_) => VerifiyScreen());
 
       case home:
         return MaterialPageRoute(builder: (_) => HomeView());
