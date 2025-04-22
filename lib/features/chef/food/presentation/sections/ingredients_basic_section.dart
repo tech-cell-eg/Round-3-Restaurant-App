756b6760c2f7e8977b92c136e7a91281845443df
DevJimmy <108478695+devJimmy990@users.noreply.github.com>
2025-04-22 05:52:16 +0200

    create add new item screen and it`s views

---
+++ lib/features/chef/food/presentation/sections/ingredients_basic_section.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/core/ui/buttons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_app/features/chef/food/controllers/ingredient_controller.dart';
+import 'package:food_app/features/chef/food/presentation/widgets/ingredient_card.dart';
+
+final List<String> _ingredients = [
+  "salt",
+  "pepper",
+  "onion",
+  "garlic",
+  "ginger",
+  "tomato",
+  "chilli",
+  "green chilli",
+  "cabbage",
+  "carrot",
+  "potato",
+  "broccoli",
+  "spinach",
+  "cauliflower",
+  "bell pepper",
+  "zucchini",
+  "eggplant",
+];
+
+class BasicIngredientsSection extends StatefulWidget {
+  final IngredientController controller;
+  const BasicIngredientsSection({super.key, required this.controller});
+
+  @override
+  State<BasicIngredientsSection> createState() =>
+      _BasicIngredientsSectionState();
+}
+
+class _BasicIngredientsSectionState extends State<BasicIngredientsSection> {
+  bool openMenu = false;
+  void toggleIngredient(String ingredient) {
+    setState(() {
+      widget.controller.value.contains(ingredient)
+          ? widget.controller.value.remove(ingredient)
+          : widget.controller.value.add(ingredient);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 14.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Basic"),
+              Buttons.text(
+                onPressed: () => setState(() => openMenu = !openMenu),
+                label: openMenu ? "close" : "see all",
+              ),
+            ],
+          ),
+        ),
+        openMenu
+            ? Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14.w),
+              child: Wrap(
+                children:
+                    _ingredients
+                        .map(
+                          (ingredient) => IngredientCard(
+                            ingredient: ingredient,
+                            isSelected: widget.controller.value.contains(
+                              ingredient,
+                            ),
+                            toggleIngredient: toggleIngredient,
+                          ),
+                        )
+                        .toList(),
+              ),
+            )
+            : SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(width: 14),
+                  ..._ingredients
+                      .take(5)
+                      .map(
+                        (ingredient) => IngredientCard(
+                          ingredient: ingredient,
+                          isSelected: widget.controller.value.contains(
+                            ingredient,
+                          ),
+                          toggleIngredient: toggleIngredient,
+                        ),
+                      ),
+                ],
+              ),
+            ),
+      ],
+    );
+  }
+}
---
+++ lib/features/chef/food/presentation/sections/ingredients_spices_section.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/core/ui/buttons.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_app/features/chef/food/controllers/ingredient_controller.dart';
+import 'package:food_app/features/chef/food/presentation/widgets/ingredient_card.dart';
+
+final List<String> _ingredients = [
+  "cumin",
+  "coriander",
+  "turmeric",
+  "cinnamon",
+  "cardamom",
+  "cloves",
+  "bay leaf",
+  "black pepper",
+  "mustard seeds",
+  "fenugreek seeds",
+  "fennel seeds",
+  "red chilli powder",
+  "garam masala",
+  "curry powder",
+];
+
+class SpicesIngredientsSection extends StatefulWidget {
+  final IngredientController controller;
+  const SpicesIngredientsSection({super.key, required this.controller});
+
+  @override
+  State<SpicesIngredientsSection> createState() =>
+      _SpicesIngredientsSectionState();
+}
+
+class _SpicesIngredientsSectionState extends State<SpicesIngredientsSection> {
+  bool openMenu = false;
+  void toggleIngredient(String ingredient) {
+    setState(() {
+      widget.controller.value.contains(ingredient)
+          ? widget.controller.value.remove(ingredient)
+          : widget.controller.value.add(ingredient);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 14.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Spices"),
+              Buttons.text(
+                onPressed: () => setState(() => openMenu = !openMenu),
+                label: openMenu ? "close" : "see all",
+              ),
+            ],
+          ),
+        ),
+        openMenu
+            ? Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14.w),
+              child: Wrap(
+                children: [
+                  ..._ingredients.map(
+                    (ingredient) => IngredientCard(
+                      ingredient: ingredient,
+                      isSelected: widget.controller.value.contains(ingredient),
+                      toggleIngredient: toggleIngredient,
+                    ),
+                  ),
+                ],
+              ),
+            )
+            : SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  const SizedBox(width: 14),
+                  ..._ingredients
+                      .take(5)
+                      .map(
+                        (ingredient) => IngredientCard(
+                          ingredient: ingredient,
+                          isSelected: widget.controller.value.contains(
+                            ingredient,
+                          ),
+                          toggleIngredient: toggleIngredient,
+                        ),
+                      ),
+                ],
+              ),
+            ),
+      ],
+    );
+  }
+}
